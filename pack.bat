fe73c45d8b4dcbad841f9cf5c429a434e8be9811
duncanlang <duncan.lang@me.com>
2024-12-26 13:46:14 -0800

    add pack.bat

---
+++ pack.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\7-Zip\7z.exe" a -tzip Backloggery-Extras.zip images/* icon16.png icon32.png icon128.png jquery-3.6.0.min.js backloggery-extras.user.js manifest.json options.html options.js package.json polyfill.js
